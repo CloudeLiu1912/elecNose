9b6e8177710c9a57de53952f50f0cbb0841d6f7d
CloudeLiu1912 <453186375@qq.com>
2018-11-04 16:46:23 +0800

    Mileo

--- conv_test.py
+++ conv_test.py
@@ -1,136 +1,23 @@
 import numpy as np
-import pandas as pd
-import time
 import tensorflow as tf
-import cv2
+import time
+import pandas as pd
 
-Program_Start = time.time()
-max_step = 3000
-batch_size = 128
-#
-# data_test = np.load('data_test.npy')
-# label_test = np.load('label_test.npy')
-# data = np.load('data.npy')
-# label = np.load('label.npy')
-#
-# print(data.shape)
-# print(label.shape)
-#
-# print(data_test.shape)
-# print(label_test.shape)
-#
-# batch1 = data[:batch_size, :, :, :]
-# print(batch1.shape)
 
-#  no dropout, conv-pool-conv-pool-fc1-fc2
+x = np.load('data.npy')  # 480x340x16
+y = np.load('label.npy')  # 480x10
 
-#  data
-x_input = tf.placeholder(tf.float32, shape=[None, 32, 32, 3], name='x_input')
-y_input = tf.placeholder(tf.float32, shape=[None, 10], name='y_input')
+indices = np.arange(480)
+np.random.shuffle(indices)
 
-# CONV : -1x32x32x3 -> -1x32x32x32
+x_in = x[indices[0]]
+y_in = y[indices[0]]
+for j in range(1, 480):
+    x_in = np.vstack((x_in, x[indices[j]]))
+    y_in = np.hstack((y_in, y[indices[j]]))
+x_in = x_in.reshape([480, 340, 16, 1])
+a = y_in.reshape([480, 10])
+y_in = pd.get_dummies(y_in)
+print(y_in.shape)
 
-kernel = tf.Variable(tf.truncated_normal([5, 5, 3, 32], dtype=tf.float32, stddev=1e-1, name='weights'))
-conv = tf.nn.conv2d(x_input, kernel, [1, 1, 1, 1], padding='SAME')
-print('conv.shape=', conv.shape)
-bias = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[32]), name='bias')
-print('bias.shape=', bias.shape)
-Wx_plus_b = tf.nn.bias_add(conv, bias)
-print('bias.shpae=', Wx_plus_b.shape)
-conv1 = tf.sigmoid(Wx_plus_b, name='conv1')  # or Use ReLu
-print('conv1.shape=', conv1.shape)
-
-pool1 = tf.nn.avg_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], name='pool1',
-                       padding='VALID')  # or Max-pool
-print('pool1.shape=', pool1.shape)
-norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
-print('norm1.shape=', norm1.shape)
-
-# CONV : -1x32x32x32 -> -1x32x32x64
-kernel = tf.Variable(tf.truncated_normal([5, 5, 32, 64], dtype=tf.float32, stddev=1e-1, name='weights'))
-conv = tf.nn.conv2d(norm1, kernel, [1, 3, 3, 1], padding='SAME')
-print('conv,shape=', conv.shape)
-bias = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[64]), name='bias')
-print('bias.shape=', bias.shape)
-Wx_plus_b = tf.nn.bias_add(conv, bias)
-print('bias.shpae=', Wx_plus_b.shape)
-conv2 = tf.sigmoid(Wx_plus_b, name='conv2')
-print('conv2.shape=', conv2.shape)
-
-pool2 = tf.nn.avg_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 1, 1, 1], name='pool2', padding='VALID')
-print('pool2.shape=', pool2.shape)
-norm2 = tf.nn.lrn(pool2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
-print('norm2.shape=', norm2.shape)
-pass
-
-# flatten
-x_flatten = tf.layers.flatten(norm2)
-print(x_flatten.shape)
-
-W_fc1 = tf.Variable(tf.truncated_normal([1024, 256], dtype=tf.float32, mean=0, stddev=1e-1), name='W_fc1')
-fc1 = tf.nn.sigmoid(tf.matmul(x_flatten, W_fc1))
-
-W_fc2 = tf.Variable(tf.truncated_normal([256, 10], dtype=tf.float32, mean=0, stddev=1e-1), name='W_fc2')
-fc2 = tf.nn.sigmoid(tf.matmul(fc1, W_fc2))
-print('fc2.shape=', fc2.shape)
-
-init = tf.global_variables_initializer()
-
-# 定义训练的loss函数。
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=fc2, labels=y_input))
-
-# 定义优化器，学习率设置为0.09，学习率可以设置为其他的数值。
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.09).minimize(loss)
-
-# 定义准确率
-valaccuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(fc2, 1), tf.argmax(y_input, 1)), tf.float32))
-
-# indices = np.arange(40000)
-# np.random.shuffle(indices)
-# a = data[0, :, :, :]
-# print(a.shape)
-# print(a)
-# b = data[1, :, :, :][np.newaxis, :]
-# c = data[2, :, :, :]
-# anew = a[np.newaxis, :]
-# bnew = b[np.newaxis, :]
-# cnew = c[np.newaxis, :]
-# d = anew
-# d = np.vstack((d, b))
-# d = np.vstack((d, cnew))
-# print(d.shape)
-
-# tmp_y = pd.get_dummies(label[100])
-# print(tmp_y.shape)
-
-# with tf.Session() as sess:
-#     sess.run(init)
-#
-#     for epoch in range(1):
-#         indices = np.arange(40000)
-#         np.random.shuffle(indices)
-#         for i in range(0, 40000, 100):
-#             x_in = []
-#             y_in = []
-#             for j in range(0, 100):
-#                 tmp_x = data[indices[i + j]][np.newaxis, :]
-#                 tmp_y = label[indices[i + j]]
-#                 if j == 0:
-#                     x_in = tmp_x
-#                     y_in = tmp_y
-#                 else:
-#                     x_in = np.vstack((x_in, tmp_x))
-#                     y_in = np.hstack((y_in, tmp_y))
-#             y_in = pd.get_dummies(y_in)
-#             a, b = sess.run([optimizer, loss], feed_dict={x_input: x_in, y_input: y_in})
-#             print("\r%lf\n" % b, end='')
-#
-#         acc = 0
-#         for i in range(0, 10000, 200):
-#             x_in = data_test[i:i + 200, :, :, :]
-#             y_in = pd.get_dummies(label_test[i:i + 200])
-#             acc += sess.run(valaccuracy, feed_dict={x_input: x_in, y_input: y_in})
-#         print("Epoch ", epoch, ': validation rate: ', acc / 50)
-
-Program_End = time.time()
-print('\n', Program_End - Program_Start)
+print(1)
--- tmp.py
+++ tmp.py
@@ -4,8 +4,8 @@ import time
 import pandas as pd
 
 
-x_in = np.load('data.npy')  # 480x340x16
-y_in = np.load('label.npy')  # 480x10
+x = np.load('data.npy')  # 480x340x16
+y = np.load('label.npy')  # 480x10
 
 # a = np.arange(0, 12, dtype=np.float32)
 # np.random.shuffle(a)
@@ -15,77 +15,96 @@ y_in = np.load('label.npy')  # 480x10
 
 
 start_time = time.time()
-
 # input_shape
 with tf.name_scope('Input'):
     x_input = tf.placeholder(tf.float32, shape=[None, 340, 16, 1], name='x_input')
     y_input = tf.placeholder(tf.float32, shape=[None, 10], name='y_input')
     pass
 
+# conv1
 with tf.name_scope('Conv1'):
-    W1 = tf.Variable(tf.truncated_normal([3, 3, 1, 16], dtype=tf.float32, stddev=1e-1, name='weights'))
-    W1x = tf.nn.conv2d(x_input, W1, [1, 1, 1, 1], padding='SAME')
-    b1 = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[16]), name='bias')
+    W1 = tf.Variable(tf.truncated_normal([4, 1, 1, 32], dtype=tf.float32, stddev=1e-1, name='weights'))
+    W1x = tf.nn.conv2d(x_input, W1, strides=[1, 1, 1, 1], padding='SAME')
+    b1 = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[32]), name='bias')
     Wx_plus_b = tf.nn.bias_add(W1x, b1)
-    conv1 = tf.sigmoid(Wx_plus_b, name='conv1')  # or Use ReLu
-    print('W1x.shape=', W1x.shape)
-    print('b1.shape=', b1.shape)
-    print('W1x+b.shpae=', Wx_plus_b.shape)
+    conv1 = tf.nn.relu(Wx_plus_b, name='conv1')  # or Use ReLu
+
+    print('conv.shape=', W1x.shape)
+    print('bias.shape=', b1.shape)
+    print('bias.shpae=', Wx_plus_b.shape)
     print('conv1_out.shape=', conv1.shape)
 
-    pool1 = tf.nn.avg_pool(conv1, ksize=[1, 11, 1, 1], strides=[1, 11, 1, 1], name='pool1',
-                           padding='VALID')  # or Max-pool
+    # pool1
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 4, 1, 1], strides=[1, 4, 1, 1], name='pool1',
+                           padding='SAME')  # or Max-pool
     print('pool1_out.shape=', pool1.shape)
 
-    # Local Response Normalization -> generalization
-    norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
+# conv2
+with tf.name_scope('Conv2'):
+    W2 = tf.Variable(tf.truncated_normal([2, 1, 32, 16], dtype=tf.float32, stddev=1e-1, name='weights'))
+    W2x = tf.nn.conv2d(pool1, W2, strides=[1, 1, 1, 1], padding='SAME')
+    b2 = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[16]), name='bias')
+    Wx_plus_b = tf.nn.bias_add(W2x, b2)
+    conv2 = tf.nn.relu(Wx_plus_b, name='conv2')  # or Use ReLu
+
+    print('conv.shape=', W2x.shape)
+    print('bias.shape=', b2.shape)
+    print('bias.shpae=', Wx_plus_b.shape)
+    print('conv1_out.shape=', conv2.shape)
+
+    # pool2
+    pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1], name='pool2',
+                           padding='SAME')  # or Max-pool
+    print('pool1_out.shape=', pool2.shape)
+
+# conv345
+with tf.name_scope('Conv2'):
+    W3 = tf.Variable(tf.truncated_normal([2, 1, 16, 16], dtype=tf.float32, stddev=1e-1, name='weights'))
+    W3x = tf.nn.conv2d(pool2, W3, [1, 1, 1, 1], padding='SAME')
+    b3 = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[16]), name='bias')
+    Wx_plus_b = tf.nn.bias_add(W3x, b3)
+    conv3 = tf.nn.relu(Wx_plus_b, name='conv3')  # or Use ReLu
+
+    W4 = tf.Variable(tf.truncated_normal([2, 1, 16, 16], dtype=tf.float32, stddev=1e-1, name='weights'))
+    W4x = tf.nn.conv2d(conv3, W4, [1, 1, 1, 1], padding='SAME')
+    b4 = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[16]), name='bias')
+    W4x_plus_b = tf.nn.bias_add(W4x, b4)
+    conv4 = tf.nn.relu(W4x_plus_b, name='conv4')  # or Use ReLu
+
+    W5 = tf.Variable(tf.truncated_normal([2, 1, 16, 16], dtype=tf.float32, stddev=1e-1, name='weights'))
+    W5x = tf.nn.conv2d(conv4, W5, [1, 1, 1, 1], padding='SAME')
+    b5 = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[16]), name='bias')
+    W5x_plus_b = tf.nn.bias_add(W5x, b5)
+    conv5 = tf.nn.relu(W5x_plus_b, name='conv5')  # or Use ReLu
+
+    # Local Response Normalization -> generalization -- 局部响应归一化
+    norm1 = tf.nn.lrn(pool2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
     print('norm1.shape=', norm1.shape)
     pass
 
-with tf.name_scope('Conv2'):
-    W2 = tf.Variable(tf.truncated_normal([3, 3, 16, 32], dtype=tf.float32, stddev=1e-1, name='weights'))
-    W2x = tf.nn.conv2d(norm1, W2, [1, 1, 1, 1], padding='SAME')
-    b2 = tf.Variable(tf.constant(0.0, dtype=tf.float32, shape=[32]), name='bias')
-    W2x_plus_b = tf.nn.bias_add(W2x, b2)
-    conv2 = tf.sigmoid(W2x_plus_b, name='conv1')  # or Use ReLu
-    print('W2x.shape=', W2x.shape)
-    print('b2.shape=', b2.shape)
-    print('W2x+b.shpae=', W2x_plus_b.shape)
-    print('conv2_out.shape=', conv2.shape)
-
-    pool2 = tf.nn.avg_pool(conv2, ksize=[1, 5, 1, 1], strides=[1, 3, 1, 1], name='pool1',
-                           padding='VALID')  # or Max-pool
-    print('pool2_out.shape=', pool2.shape)
-
-    # Local Response Normalization -> generalization
-    norm2 = tf.nn.lrn(pool2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
-    print('norm2.shape=', norm2.shape)
-    pass
-
 # flatten
-x_flatten = tf.layers.flatten(norm2)
+x_flatten = tf.layers.flatten(norm1)
 print('x_flatten.shape=', x_flatten.shape)
 
-#
-W_fc1 = tf.Variable(tf.truncated_normal([4068, 1024], dtype=tf.float32, mean=0, stddev=1e-1), name='W_fc1')
-fc1 = tf.nn.sigmoid(tf.matmul(x_flatten, W_fc1))
+# FC relu
+W_fc1 = tf.Variable(tf.truncated_normal([11008, 1024], dtype=tf.float32, mean=0, stddev=1e-1), name='W_fc1')
+fc1 = tf.nn.relu(tf.matmul(x_flatten, W_fc1))
+print('fc.shape=', fc1.shape)
 
-W_fc2 = tf.Variable(tf.truncated_normal([1024, 256], dtype=tf.float32, mean=0, stddev=1e-1), name='W_fc2')
-fc2 = tf.nn.sigmoid(tf.matmul(fc1, W_fc2))
+# softmax
+W_s = tf.Variable(tf.truncated_normal([1024, 10], dtype=tf.float32, mean=0, stddev=1e-1), name='W_softmax')
+b = tf.Variable(initial_value=tf.zeros(shape=[10]), name='bias')
+res = tf.nn.softmax(tf.add(tf.matmul(fc1, W_s), b))
+print('softmax.shape=', res.shape)
 
-W_fc3 = tf.Variable(tf.truncated_normal([256, 10], dtype=tf.float32, mean=0, stddev=1e-1), name='W_fc2')
-fc3 = tf.nn.sigmoid(tf.matmul(fc1, W_fc3))
-print('fc_out.shape=', fc3.shape)
-
-#
-y_predict = fc3
+y_predict = res
 
 with tf.name_scope('Loss'):
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_in, logits=y_predict))
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_input, logits=y_predict))
 with tf.name_scope('Train'):
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 with tf.name_scope('Accuracy'):
-    correct_prediction = tf.equal(tf.argmax(y_in, 1), tf.argmax(y_predict, 1))
+    correct_prediction = tf.equal(tf.argmax(y_input, 1), tf.argmax(y_predict, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 init = tf.global_variables_initializer()
@@ -93,34 +112,46 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
 
-    for epoch in range(1):
+    for epoch in range(10000):
         indices = np.arange(480)
         np.random.shuffle(indices)
-        for i in range(0, 480, 20):
-            x_in = []
-            y_in = []
-            for j in range(0, 100):
-                tmp_x = x_in[indices[i + j]][np.newaxis, :]
-                tmp_y = y_in[indices[i + j]]
-                if j == 0:
-                    x_in = tmp_x
-                    y_in = tmp_y
-                else:
-                    x_in = np.vstack((x_in, tmp_x))
-                    y_in = np.hstack((y_in, tmp_y))
-            y_in = pd.get_dummies(y_in)
-            a, b = sess.run([optimizer, loss], feed_dict={x_input: x_in, y_input: y_in})
-            print("\r%lf\n" % b, end='')
+        # for i in range(0, 480, 20):
+        # x_in = []
+        # y_in = []
+        # for j in range(100):
+        #     tmp_x = x_in[indices[i + j]][np.newaxis, :]
+        #     tmp_y = y_in[indices[i + j]]
+        #     if j == 0:
+        #         x_in = tmp_x
+        #         y_in = tmp_y
+        #     else:
+        #         x_in = np.vstack((x_in, tmp_x))
+        #         y_in = np.hstack((y_in, tmp_y))
+        # y_in = pd.get_dummies(y_in)
+
+        x_in = x[indices[0]]
+        y_in = y[indices[0]]
+        for j in range(1, 480):
+            x_in = np.vstack((x_in, x[indices[j]]))
+            y_in = np.hstack((y_in, y[indices[j]]))
+        x_in = x_in.reshape([480, 340, 16, 1])
+        y_in = y_in.reshape([480, 10])
+        print(y_in.shape)
+
+        a, b = sess.run([optimizer, cross_entropy], feed_dict={x_input: x_in[:400], y_input: y_in[:400]})
+        print("\r%lf\n" % b, end='')
 
         acc = 0
-        for i in range(0, 10000, 200):
-            x_in = data_test[i:i + 200, :, :, :]
-            y_in = pd.get_dummies(label_test[i:i + 200])
-            acc += sess.run(valaccuracy, feed_dict={x_input: x_in, y_input: y_in})
-        print("Epoch ", epoch, ': validation rate: ', acc / 50)
+        # for i in range(80):
+        #     x_in = x[i:i + 200, :, :, :]
+        #     y_in = pd.get_dummies(y_in[i:i + 200])
+        acc += sess.run(accuracy, feed_dict={x_input: x_in[400:], y_input: y_in[400:]})
+        print("Epoch ", epoch, 'Loss: ', b, ' validation rate: ', acc)
 
 
 end_time = time.time()
+
+print('Total time: ', end_time-start_time)
 print(1)
 
 
